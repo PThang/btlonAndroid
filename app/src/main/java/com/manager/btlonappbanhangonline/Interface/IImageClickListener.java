db4fa7702bd9d84aeb636be93c06aea98d13bb40
GiapQuang <89502309+GiapQuang@users.noreply.github.com>
2023-03-14 20:33:23 +0700

    Delete IImageClickListener.java

--- app/src/main/java/com/manager/btlonappbanhangonline/Interface/IImageClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.manager.btlonappbanhangonline.Interface;
-
-import android.view.View;
-
-public interface IImageClickListener {
-    void onImageClick(View view, int pos, int giatri);
-}
